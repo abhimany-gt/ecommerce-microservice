c06abe4076d5a6aaa08e091534e6f5c3b380fd54
abhigupta-k <66472260+abhigupta-k@users.noreply.github.com>
2020-06-11 12:12:41 +0530

    added changes

---
+++ order-service/src/main/java/com/birlasoft/api/controller/Interceptor.java
@@ -0,0 +1,39 @@
+package com.birlasoft.api.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class Interceptor implements HandlerInterceptor{
+	static Claims claims ;
+	static String username;
+	static String authority;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		String header = request.getHeader("Authorization");
+		String token = header.replace("Bearer", "");
+		claims = Jwts.parser()
+	             .setSigningKey("secret-key".getBytes())
+	             .parseClaimsJws(token)
+	             .getBody();
+		username = claims.getSubject();
+		//System.out.println(username);
+        List<Map<String, String>> authoritiesMap=(List<Map<String, String>>) claims.get("auth");
+        Map<String,String> map =authoritiesMap.get(0);
+        authority =map.get("authority");
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	
+}
+
