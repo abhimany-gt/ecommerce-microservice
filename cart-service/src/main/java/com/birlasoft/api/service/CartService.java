a3f0d1dbd73a0f3cb479df3b6ad3d453ae85bac3
abhimany-gt <66764868+abhimany-gt@users.noreply.github.com>
2020-06-16 15:01:13 +0530

    Update CartService

--- cart-service/src/main/java/com/birlasoft/api/service/CartService.java
+++ cart-service/src/main/java/com/birlasoft/api/service/CartService.java
@@ -35,29 +35,29 @@ public class CartService {
 
 	static Logger log = LoggerFactory.getLogger(CartService.class);
 
-	
 	public Cart addInCart(Request request) {
 
-		//find baseurl from zuul-service
+		// find baseurl from zuul-service
 		List<ServiceInstance> instances = discoveryClient.getInstances("zuul-service");
 		String baseUrl = instances.get(0).getUri().toString();
 		String producturl = baseUrl + "/" + "products/" + request.getProductId();
-		
-		//add token in header
+
+		// add token in header
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 		headers.add("Authorization", "Bearer " + Interceptor.getToken());
-		
-		//call rest template /product/{id}
+
+		// call rest template /product/{id}
 		ResponseEntity<Product> response = restTemplate.exchange(producturl, HttpMethod.GET,
 				new HttpEntity<>("parameters", headers), Product.class);
-		
+
 		log.info("Respnse from product/{id}: " + response);
 		// create cart object
 		Product product = response.getBody();
 		Cart cart = new Cart();
 		cart.setProductId(product.getId());
 		cart.setUserName(Interceptor.getUsername());
+		cart.setPrice(product.getPrice());
 		return repository.save(cart);
 
 	}
@@ -68,7 +68,10 @@ public class CartService {
 	}
 
 	public Optional<Cart> getByUserName(String userName) {
-		return repository.findByUserName(userName);
+
+		Optional<Cart> response = repository.findByUserName(userName);
+
+		return response;
 	}
 
 }
