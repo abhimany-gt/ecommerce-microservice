62ae9860e70c6d5d78d9d3b0d125879d6b06693f
abhimany-gt <66764868+abhimany-gt@users.noreply.github.com>
2020-06-16 15:06:36 +0530

    add changes

--- order-service/src/main/java/com/birlasoft/api/commons/Cart.java
+++ order-service/src/main/java/com/birlasoft/api/commons/Cart.java
@@ -5,46 +5,21 @@ public class Cart {
 	private int cartId;
 	private int productId;
 	private String userName;
-	public Cart(int cartId, int productId, String userName) {
-		super();
-		this.cartId = cartId;
-		this.productId = productId;
-		this.userName = userName;
-	}
-	public Cart() {
-		super();
-	}
-	public int getCartId() {
-		return cartId;
-	}
-	public void setCartId(int cartId) {
-		this.cartId = cartId;
-	}
-	public int getProductId() {
-		return productId;
-	}
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	@Override
-	public String toString() {
-		return "Cart [cartId=" + cartId + ", productId=" + productId + ", userName=" + userName + "]";
-	}
+	private double price;
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + cartId;
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
 		result = prime * result + productId;
 		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -56,6 +31,8 @@ public class Cart {
 		Cart other = (Cart) obj;
 		if (cartId != other.cartId)
 			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
 		if (productId != other.productId)
 			return false;
 		if (userName == null) {
@@ -65,6 +42,54 @@ public class Cart {
 			return false;
 		return true;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "Cart [cartId=" + cartId + ", productId=" + productId + ", userName=" + userName + ", price=" + price
+				+ "]";
+	}
+
+	public int getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Cart() {
+		super();
+	}
+
+	public Cart(int cartId, int productId, String userName, double price) {
+		super();
+		this.cartId = cartId;
+		this.productId = productId;
+		this.userName = userName;
+		this.price = price;
+	}
 }
